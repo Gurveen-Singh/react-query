320220e0ecc456ee95b04bafd6d7439128bf5eeb
Gurveen <gurveensingh45@gmail.com>
2022-04-26 14:40:56 +0530

    Leant Fetching in React Query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,41 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const {
+    isLoading,
+    data: heroData,
+    error,
+    isError,
+    isFetching,
+  } = useQuery("super-heroes", fetchSuperHeroes, {
+    // cacheTime: 1000,
+    // staleTime: 5000,
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    // refetchInterval: 1000,
+    // refetchIntervalInBackground: 100,
+  });
+
+  console.log(isLoading, isFetching);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error: {error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {heroData?.data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
+};
--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,27 +1,38 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const SuperHeroesPage = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then(res => {
-      setData(res.data)
-      setIsLoading(false)
-    })
-  }, [])
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error.message}</h2>;
   }
 
   return (
     <>
       <h2>Super Heroes Page</h2>
-      {data.map(hero => {
-        return <div>{hero.name}</div>
+      {data.map((hero, ind) => {
+        return <div key={ind}>{hero.name}</div>;
       })}
     </>
-  )
-}
+  );
+};
